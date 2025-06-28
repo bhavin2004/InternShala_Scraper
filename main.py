e4259d82b7e7b1562d5504d359368e14580ed4a2
Bhavin Karangia <94212977+bhavin2004@users.noreply.github.com>
2025-06-28 07:35:48 +0000

    added sample option in ui

--- main.py
+++ main.py
@@ -1,51 +1,74 @@
-from datetime import datetime
 import streamlit as st
-import os
-from IntenShalla_Scraper import main
-import pandas as pd
-from streamlit_option_menu import option_menu
-st.set_page_config(
-    page_title="Scrapper",
-    layout='centered'
+from scrape import (
+    scrape_website,
+    extract_body_content,
+    clean_body_content,
+    split_dom_content,
 )
-with st.sidebar:
-    selection = option_menu(
-        menu_title="Menu",                      # Title of the menu
-        options=["Home", "Download Previous File", "About Project"],  # Menu options
-        icons=["house", "filetype-csv", "file-earmark-person-fill"],  # Optional icons (from FontAwesome)
-        menu_icon="list",                      # Main menu icon
-        default_index=1,                       # Default selected item
-        orientation="vertical",                # Options: "horizontal" or "vertical"
-    )
-    
-if selection=='Home':
-    st.title("Intenshalla Internship And Job Scrapper")
+from parse import parse_with_custom_model
+import pandas as pd
 
-    # st.write("Job Name")
-    job_name=st.text_input("👔 Job Name",placeholder='Enter the Job You Want To Scrap',icon='💼')
-    limit=int(st.number_input("No. Of Pages to Scrape",1,value=1,icon='📄'))
-    if st.button("Begin Scraping",type='secondary',icon='🔎'):
-        if job_name:
-            date = datetime.now().strftime("%d_%m_%Y")
-            output_file=f"files/{job_name}_{date}.csv"
-            main(job_name,output_file,limit)
-            st.success("Scraped the Data Scuccessfully")
-            df=pd.read_csv(output_file)
-            st.download_button('Dowmload CSV',data=df.to_csv(index=False),mime='text/plain',file_name=output_file)
-            with st.expander("View Data"):
-                st.write(df)
+samples={
+        'https://www.scrapethissite.com/pages/simple/':'Get the name of Country,Capital,Population and area',
+        'https://quotes.toscrape.com/':'Get all the quotes,tags and author names',
+        'https://books.toscrape.com/':'Get the book title,price and is available in stock or not',
+        'https://webscraper.io/test-sites/e-commerce/allinone':'Get the product name,price and description',
+        }
+
+# Streamlit UI
+st.set_page_config(page_title="AI Web Scraper", page_icon=":mag_right:")
+st.title("AI Web Scraper")
+
+selection=''
+if st.toggle('Get Sample'):
+    selection = int(st.selectbox('Select a sample website to scrape', list(f"Sample {i}" for i in range(1, len(samples)+1)), format_func=lambda x: f"Sample {x.split()[-1]}").split(' ')[-1])
+
+# print(list(samples.keys())[selection-1])
+url = st.text_input("Enter Website URL",value=list(samples.keys())[selection-1] if selection else '', placeholder="https://example.com")
+
+
+# Step 1: Scrape the Website
+scrape=st.button("Scrape Website")
+if scrape:
+    if url:
+        # st.write("Scraping the website...")
+        with st.spinner("Scraping in progress..."):
+            page_source = scrape_website(url)
+            body_content = extract_body_content(page_source) 
+            clean_content = clean_body_content(body_content)
+            
+            st.session_state.dom_content = clean_content
+        if page_source:
+            st.success("Website scraped successfully!")
+            with st.expander("View Screenshot"):
+                st.image('screenshot.png', caption='Screenshot of the website')
+            with st.expander("View Cleaned Content"):
+                st.text_area("Cleaned Content", value=clean_content, height=300)
         else:
-            st.warning("Please Enter Job Name")
-elif selection=='Download Previous File':
-    st.title("Download Previous Scraped Files")
-    os.makedirs("files",exist_ok=True)
+            st.error("Failed to scrape the website. Please check the URL or your internet connection.")
+    else:
+        st.error("Please enter a valid URL.")       
+        
+if 'dom_content' in st.session_state:
     
-    files_list=os.listdir('files/')
-    file = st.selectbox("☰ Select a file to download", files_list)
-
-    df = pd.read_csv(f'files/{file}')
-    with st.expander("View Data",icon='👀'):
-        st.write(df)
+    parse_desription = st.text_area("Describe what you want to extract from the content",value=list(samples.values())[selection-1] if selection else "") +"\n get data in csv format"
     
-    st.download_button('Download CSV',data=df.to_csv(index=False),mime='text/plain',file_name=file,icon='⬇️')
-    # czxczx
+    if st.button("Parse Content"):
+        if parse_desription:
+            with st.spinner("Parsing in progress..."):
+                
+                dom_chunks = split_dom_content(st.session_state.dom_content)
+                
+                parse_with_custom_model(dom_chunks, parse_desription)
+                df = pd.read_csv('parsed_output.csv',sep=';:;', engine='python')
+                st.dataframe(df)
+                st.download_button(
+                    label="Download Parsed CSV",
+                    data=df.to_csv(sep=';:;', index=False).encode('utf-8'),
+                    file_name='parsed_output.csv',
+                    mime='text/csv',
+                )
+                
+                
+                
+        
